77c1560169cd55f108260c5df1c9e0f5157c9ff7
xdjeremy <jeremy.fran@icloud.com>
2024-04-23 11:18:14 +0800

    add input component

---
+++ src/stories/components/Input.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Input } from '@/components/ui/input'
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  tags: ['autodocs'],
+  render: (args) => <Input {...args} />,
+  args: {
+    placeholder: 'Placeholder',
+    disabled: false,
+  },
+}
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
