125cbf380e4c213e234a4a36fb20ab540b826e56
xdjeremy <jeremy.fran@icloud.com>
2024-04-24 16:03:42 +0800

    feat(auth): check if user already logged in

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,10 +1,20 @@
 import React, { FC, ReactNode } from 'react'
+import { isUserLoggedIn } from '@/utils/auth-checks'
+import { createServerClient } from '@/utils/supabase'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 interface Props {
   children: ReactNode
 }
 
-const AuthLayout: FC<Props> = ({ children }: { children: ReactNode }) => {
+const AuthLayout: FC<Props> = async ({ children }: { children: ReactNode }) => {
+  // check if user is already logged in
+  const supabase = createServerClient(cookies())
+  if (await isUserLoggedIn(supabase)) {
+    // redirect to dashboard if user is already logged in
+    redirect('/')
+  }
   return <div className={'max-h-screen w-full overflow-hidden'}>{children}</div>
 }
 
