8c7fd10f4769c8d80805a0034041b9508f3c2c15
xdjeremy <jeremy.fran@icloud.com>
2024-04-24 09:39:53 +0800

    fix: login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import LoginForm from '@/app/auth/login/login-form'
+import VectorBackground from '@/app/auth/components/vector-background'
+
+const LoginPage = () => {
+  return (
+    <div className={'flex flex-row justify-between'}>
+      <VectorBackground
+        className={'absolute -left-56 bottom-64 hidden 2xl:block'}
+      />
+      <div
+        className={
+          'flex min-h-screen w-full flex-col items-center justify-center 2xl:w-1/3 2xl:pr-14 '
+        }
+      >
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
