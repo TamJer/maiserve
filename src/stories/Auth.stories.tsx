0e758d00dfb6a26357f025e9ddabb6588202a048
xdjeremy <jeremy.fran@icloud.com>
2024-04-25 05:35:44 +0800

    feat(auth): add forgot password page

--- src/stories/Auth.stories.tsx
+++ src/stories/Auth.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
+import ForgotPasswordPage from '@/app/auth/forgot-password/page'
 
 const meta: Meta = {
   title: 'Pages/Auth',
@@ -9,4 +10,8 @@ type Story = StoryObj<typeof meta>
 
 export const Login: Story = {
   render: () => <>Test</>,
+}
+
+export const ForgotPassword: Story = {
+  render: () => <ForgotPasswordPage />,
 }
