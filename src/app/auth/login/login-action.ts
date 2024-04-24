508eb6b4edf772a30056cd1beab5653ef8ca0790
xdjeremy <jeremy.fran@icloud.com>
2024-04-24 15:30:14 +0800

    feat(auth): add login form submit

---
+++ src/app/auth/login/login-action.ts
@@ -0,0 +1,22 @@
+'use server'
+import { z } from 'zod'
+import LoginSchema from '@/app/auth/login/login-schema'
+import { createServerClient } from '@/utils/supabase'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+const loginAction = async (formData: z.infer<typeof LoginSchema>) => {
+  const supabase = createServerClient(cookies())
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email: formData.email,
+    password: formData.password,
+  })
+
+  if (error) {
+    redirect('/login?message=Could not authenticate user')
+  }
+
+  redirect('/')
+}
+export default loginAction
