3580e71150171c6b6db36246c85132fcfea2aef0
tamrivera05 <131436068+tamrivera05@users.noreply.github.com>
2024-05-03 02:57:31 +0000

    fix: Reset password schema (#51)

--- src/app/auth/reset-password/reset-password-schema.ts
+++ src/app/auth/reset-password/reset-password-schema.ts
@@ -3,9 +3,7 @@
 import { z } from 'zod'
 
 const ResetSchema = z.object({
-  email: z.string().min(2, {
-    message: 'Email must be at least 2 characters.',
-  }),
+  email: z.string().email(),
 })
 
 export default ResetSchema
